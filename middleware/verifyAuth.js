66964e8767beb30953eaa8594c696a1dd1280a2c
hamabarznji <hama.aak1@gmail.com>
2022-06-14 21:13:57 +0300

    fiex unauthorised msg

--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -2,14 +2,19 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = async (req, res, next) => {
     const { authorization } = req.headers;
-    if (!authorization) return res.status(401).send("Unauthorized");
+    if (!authorization)
+        return res
+            .status(401)
+            .send(
+                "<h1 style='text-align:center'>Access Denied! You are not allowed to access this api(Unauthorized).</h1>"
+            );
     const token = authorization.split(" ")[1];
     try {
         jwt.verify(token, "fyp");
         next();
     } catch (error) {
         res.status(401).send(
-            "Access Denied! You are not allowed to access this api(Unauthorized)."
+            "<h1 style='text-align:center'>Access Denied! You are not allowed to access this api(Unauthorized).</h1>"
         );
     }
 };
